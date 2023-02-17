aa85ba8c54168e9e3181b5d419c8b11ef1766ce7
Romarico23 <108353329+Romarico23@users.noreply.github.com>
2023-02-17 14:13:51 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 function App() {
